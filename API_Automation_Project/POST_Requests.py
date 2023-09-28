1206fd2b9876b078c34d37c1b2738a6a78f3cee5
adhungan <ankit.dhungana67@gmail.com>
2023-09-28 12:24:44 +0530

    PUT Requests

--- API_Automation_Project/POST_Requests.py
+++ API_Automation_Project/POST_Requests.py
@@ -14,6 +14,44 @@ def create_random_email():
     email = random_string + domain
     return email
 
-data =
 
-# PUT Request
+# Data for POST Request
+
+data = {
+    "name": "Ankit" + " ".join(random.choice(string.digits)),
+    "email": create_random_email(),
+    "gender": "male",
+    "status": "active"
+}
+
+# Base URL
+base_url = "https://gorest.co.in/"
+
+# Auth Token
+auth_token = "Bearer b013e6fd81e30a072c13097ca76f9d87503223970574da8fadeac0370a8d8628"
+
+def common_assertion(url, headers):
+    response = requests.post(url, json=data, headers=headers)
+    assert response.status_code == 201
+    assert response.elapsed.total_seconds() * 1000 < 500
+    assert len(response.content) < 4 * 1024
+    print("\n******** ALL ASSERTIONS PASSED ********")
+    return response.json()
+
+
+
+# POST Requests
+
+def create_user():
+    url = base_url + "public/v2/users"
+    print("\nCreate a new User URL : ", url)
+    headers = {"Authorization": auth_token}
+    json_data = json.dumps(common_assertion(url, headers=headers), indent=4)
+    print(json_data)
+    print("\n******** POST Requsst Completed ********")
+
+
+
+# Call Methods
+
+create_user()
